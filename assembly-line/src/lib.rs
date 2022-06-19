24dac2eaa35b80b6d9721ecfdefe58e03f17f8e1
Saad Ahmed <saadahm.siddiqui@gmail.com>
2022-06-20 03:07:19 +0500

    Ez

--- assembly-line/src/lib.rs
+++ assembly-line/src/lib.rs
@@ -3,9 +3,19 @@
 #![allow(unused)]
 
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    unimplemented!("calculate hourly production rate at speed: {}", speed)
+    let production_per_hour: u8 = 221;
+    let expected_production: f64 = (production_per_hour as f64) * (speed as f64);
+
+    match speed {
+        1..=4 => expected_production * 1.0,
+        5..=8 => expected_production * 0.9,
+        9..=10 => expected_production * 0.77,
+        _ => expected_production * 0.0
+    }
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    unimplemented!("calculate the amount of working items at speed: {}", speed)
+    let production_per_hour = production_rate_per_hour(speed) as u32;
+    let ppm: u32 = production_per_hour / 60;
+    return ppm;
 }
