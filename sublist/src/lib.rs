11fa3c0715dadea3ca71b3139d1e2cc350c7fc07
Saad Ahmed <saadahm.siddiqui@gmail.com>
2022-07-01 03:55:58 +0500

    Sublist solution

--- sublist/src/lib.rs
+++ sublist/src/lib.rs
@@ -1,5 +1,3 @@
-use std::cmp;
-
 #[derive(Debug, PartialEq)]
 pub enum Comparison {
     Equal,
@@ -9,55 +7,58 @@ pub enum Comparison {
 }
 
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    let mut _second_iterator = 0;
-    let mut _match_count = 0;
-
-    while _second_iterator < _second_list.len() {
-        
-        let mut _first_iterator = 0;
-        let mut _sec = _second_iterator;
-        let mut mc = 0;
-        while _sec < _second_list.len() && _first_iterator < _first_list.len() {
-            if _first_list[_first_iterator].eq(&_second_list[_sec]) {
-                mc = mc + 1;
-                _first_iterator = _first_iterator + 1;
-                _sec = _sec + 1;
-            } else {
-                _first_iterator = _first_list.len() + 1;
-                _sec = _second_list.len() + 1
-            }
-        }
-
-        if mc == _first_list.len() || mc == _second_list.len() {
-            _match_count = cmp::max(_match_count, mc);
-            break;
-        }
-
-        _match_count = cmp::max(_match_count, mc);
-        _second_iterator = _second_iterator + 1;
-    }
-
-    if _match_count == 0 {
-        if _first_list.len() == 0 && _second_list.len() != 0 {
-            return Comparison::Sublist;
-        }
-        if _first_list.len() != 0 && _second_list.len() == 0 {
-            return Comparison::Superlist;
-        }
-    }
-
-    if _match_count == _first_list.len() && _match_count == _second_list.len() {
-        return Comparison::Equal;
-    }
-
-    if _match_count == _first_list.len() {
+    if _first_list.len() == 0 && _second_list.len() != 0 {
         return Comparison::Sublist;
     }
-
-    if _match_count == _second_list.len() {
+    if _first_list.len() != 0 && _second_list.len() == 0 {
         return Comparison::Superlist;
     }
 
-    return Comparison::Unequal;
+    let mut _second_list_iterator = 0;
 
+    while _second_list_iterator < _second_list.len() {
+        let mut _first_list_iterator = 0;
+        let mut _matched_since_si = 0;
+        let mut _shadow_second = _second_list_iterator;
+        if _first_list.len() > _second_list.len() {
+            while _first_list[_first_list_iterator].ne(&_second_list[_shadow_second]) && _first_list_iterator < _first_list.len() {
+                _first_list_iterator = _first_list_iterator + 1;
+            }
+        }
+        while _first_list_iterator < _first_list.len() {
+            if _first_list[_first_list_iterator].eq(&_second_list[_shadow_second]) {
+                _first_list_iterator = _first_list_iterator + 1;
+                _shadow_second = _shadow_second + 1;
+                _matched_since_si = _matched_since_si + 1;
+            } else {
+                _first_list_iterator = _first_list.len() + 1;
+            }
+            if _first_list_iterator == _first_list.len() {
+                _first_list_iterator = _first_list.len() + 1;
+            }
+            if _shadow_second == _second_list.len() {
+                _first_list_iterator = _first_list.len() + 1;
+            }
+        }
+
+
+        if _matched_since_si == _first_list.len() || _matched_since_si == _second_list.len() {
+            if _matched_since_si == _first_list.len() && _matched_since_si == _second_list.len() {
+                return Comparison::Equal;
+            }
+
+            if _matched_since_si == _first_list.len() {
+                return Comparison::Sublist;
+            }
+
+
+            return Comparison::Superlist;
+        }
+
+
+        _second_list_iterator = _second_list_iterator + 1;
+
+    }
+
+    return Comparison::Unequal;
 }
