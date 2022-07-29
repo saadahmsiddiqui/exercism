53e04bda96c8c2bf3880ea11b7de92f6ce3d7fb7
Saad Ahmed <saadahm.siddiqui@gmail.com>
2022-07-30 00:58:22 +0500

    s

--- tournament/src/lib.rs
+++ tournament/src/lib.rs
@@ -1,17 +1,18 @@
+use std::cmp::Ordering;
+
 enum MatchState {
     WIN,
     LOSE,
     DRAW
 }
-
-// struct TallyStats {
-//     team_name: String,
-//     matches_played: String,
-//     won: u16,
-//     drawn: u16,
-//     lost: u16,
-//     points: u16,
-// }
+struct TallyStats {
+    team_name: String,
+    matches_played: String,
+    won: u16,
+    drawn: u16,
+    lost: u16,
+    points: u16,
+}
 
 pub fn parse_lines(match_results: &str) -> Vec<String> {
     let mut all_lines: Vec<String> = Vec::new();
@@ -66,11 +67,30 @@ fn parse_match_result(match_result: &String) -> Option<MatchState> {
 
 pub fn tally(match_results: &str) -> String {
     let lines = parse_lines(match_results);
-    // let teams: Vec<TallyStats> = Vec::new();
+    let teams: Vec<TallyStats> = Vec::new();
 
     for line in lines.iter() {
         let vals = parse_semi_color_separated_values(line.clone());
-        println!("Parsed: {} {} {}\n", &vals[0], &vals[1], &vals[2]);
+
+        let team_one_name = &vals[0];
+        let team_two_name = &vals[1];
+        let team_one_state = parse_match_result(&vals[2]).unwrap();
+
+        let mut team_one = teams.iter().find(|x| {
+            return x.team_name.cmp(&team_one_name) == Ordering::Equal;
+        }).unwrap();
+
+        let mut team_two = teams.iter().find(|x| {
+            return x.team_name.cmp(&team_two_name) == Ordering::Equal;
+        }).unwrap();
+
+
+        match team_one_state {
+            MatchState::DRAW => {},
+            MatchState::LOSE => {},
+            MatchState::WIN => {},
+        }
+
     }
 
     unimplemented!(
