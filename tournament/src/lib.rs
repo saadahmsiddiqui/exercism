208d67e42dcf8a8d1f0b2943bfaf0b68076f2a83
Saad Ahmed <saadahm.siddiqui@gmail.com>
2022-07-30 01:54:54 +0500

    algo

--- tournament/src/lib.rs
+++ tournament/src/lib.rs
@@ -3,11 +3,11 @@ use std::cmp::Ordering;
 enum MatchState {
     WIN,
     LOSE,
-    DRAW
+    DRAW,
 }
 struct TallyStats {
     team_name: String,
-    matches_played: String,
+    matches_played: u16,
     won: u16,
     drawn: u16,
     lost: u16,
@@ -16,11 +16,11 @@ struct TallyStats {
 
 pub fn parse_lines(match_results: &str) -> Vec<String> {
     let mut all_lines: Vec<String> = Vec::new();
-    
+
     let as_bytes = match_results.as_bytes();
 
     let mut start_index: usize = 0;
-    
+
     for index in 0..as_bytes.len() {
         if as_bytes[index] == b'\n' || index + 1 == as_bytes.len() {
             let line_vec = Vec::from(&as_bytes[start_index..index + 1]);
@@ -43,8 +43,8 @@ pub fn parse_semi_color_separated_values(raw_str: String) -> Vec<String> {
     for i in 0..as_bytes.len() {
         if as_bytes[i] == b';' || i + 1 == as_bytes.len() {
             let value = match i + 1 == as_bytes.len() {
-                true => Vec::from(&raw_str[index..i+1]),
-                false => Vec::from(&raw_str[index..i])
+                true => Vec::from(&raw_str[index..i]),
+                false => Vec::from(&raw_str[index..i]),
             };
             let utf8_str = String::from_utf8(value).unwrap();
             values_vec.push(utf8_str);
@@ -56,18 +56,26 @@ pub fn parse_semi_color_separated_values(raw_str: String) -> Vec<String> {
 }
 
 fn parse_match_result(match_result: &String) -> Option<MatchState> {
-    match match_result.as_str() {
-        "win" => Some(MatchState::WIN),
-        "loss" => Some(MatchState::LOSE),
-        "draw" => Some(MatchState::DRAW),
-        _ => None
-    }
-}
+    println!("{} {}", match_result, match_result.eq("win"));
 
+    if match_result.eq(&String::from("win")) {
+        return Some(MatchState::WIN);
+    }
+    
+    if match_result.eq(&String::from("loss")) {
+        return Some(MatchState::LOSE);
+    }
+
+    if match_result.eq(&String::from("draw")) {
+        return Some(MatchState::DRAW);
+    }
+
+    return None;
+}
 
 pub fn tally(match_results: &str) -> String {
     let lines = parse_lines(match_results);
-    let teams: Vec<TallyStats> = Vec::new();
+    let mut teams: Vec<TallyStats> = Vec::new();
 
     for line in lines.iter() {
         let vals = parse_semi_color_separated_values(line.clone());
@@ -76,21 +84,49 @@ pub fn tally(match_results: &str) -> String {
         let team_two_name = &vals[1];
         let team_one_state = parse_match_result(&vals[2]).unwrap();
 
-        let mut team_one = teams.iter().find(|x| {
-            return x.team_name.cmp(&team_one_name) == Ordering::Equal;
-        }).unwrap();
+        match teams.iter_mut().find(|team| {
+            team.team_name.cmp(team_one_name) == Ordering::Equal
+                || team.team_name.cmp(team_two_name) == Ordering::Equal
+        }) {
+            Some(team) => {
+                team.matches_played = team.matches_played + 1;
 
-        let mut team_two = teams.iter().find(|x| {
-            return x.team_name.cmp(&team_two_name) == Ordering::Equal;
-        }).unwrap();
+                if team_one_name.cmp(&team.team_name) == Ordering::Equal {
+                    match team_one_state {
+                        MatchState::WIN => {
+                            team.won = team.won + 1;
+                            team.points = team.points + 3;
+                        }
+                        MatchState::LOSE => {
+                            team.lost = team.lost + 1;
+                        }
+                        MatchState::DRAW => {
+                            team.drawn = team.drawn + 1;
+                        }
+                    }
+                }
+                if team_two_name.cmp(&team.team_name) == Ordering::Equal {
+                    match team_one_state {
+                        MatchState::WIN => {
+                            team.lost = team.lost + 1;
+                        }
+                        MatchState::LOSE => {
+                            team.points = team.points + 3;
+                            team.won = team.won + 1;
+                        }
+                        MatchState::DRAW => {
+                            team.drawn = team.drawn + 1;
+                        }
+                    }
+                }
+            }
+            None => {}
+        };
+    }
 
 
-        match team_one_state {
-            MatchState::DRAW => {},
-            MatchState::LOSE => {},
-            MatchState::WIN => {},
-        }
-
+    for team in teams.iter() {
+        println!("Team: {} Points: {} Played: {} Won: {} Lost: {} Drawn: {}", &team.team_name, &team.points, &team.matches_played, &team.won, &team.lost, &team.drawn);
     }
 
     unimplemented!(
