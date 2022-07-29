9c2934a068e30dc7c205c6543c244002fe6fa97b
Saad Ahmed <saadahm.siddiqui@gmail.com>
2022-07-30 00:25:02 +0500

    raw

--- tournament/src/lib.rs
+++ tournament/src/lib.rs
@@ -4,7 +4,7 @@
 //     DRAW
 // }
 
-// struct TeamPerformance {
+// struct TallyStats {
 //     team_name: String,
 //     matches_played: String,
 //     won: u16,
@@ -18,13 +18,15 @@ pub fn parse_lines(match_results: &str) -> Vec<String> {
     
     let as_bytes = match_results.as_bytes();
 
-    let start_index: usize = 0;
+    let mut start_index: usize = 0;
+    
     for index in 0..as_bytes.len() {
-        if as_bytes[index] == b'\n' {
-            let line_vec = Vec::from(&as_bytes[start_index..index]);
+        if as_bytes[index] == b'\n' || index + 1 == as_bytes.len() {
+            let line_vec = Vec::from(&as_bytes[start_index..index + 1]);
             let line = String::from_utf8(line_vec).unwrap();
 
             all_lines.push(line);
+            start_index = index + 1;
         }
     }
 
@@ -36,8 +38,9 @@ pub fn parse_semi_color_separated_values(raw_str: String) -> Vec<String> {
     let as_bytes = raw_str.as_bytes();
 
     let mut index: usize = 0;
+
     for i in 0..as_bytes.len() {
-        if as_bytes[i] == b';' || as_bytes[i] == b'\n' {
+        if as_bytes[i] == b';' || as_bytes[i] == b'\n' || i + 1 == as_bytes.len() {
             let value = Vec::from(&raw_str[index..i]);
             let utf8_str = String::from_utf8(value).unwrap();
             values_vec.push(utf8_str);
@@ -51,15 +54,12 @@ pub fn parse_semi_color_separated_values(raw_str: String) -> Vec<String> {
 
 pub fn tally(match_results: &str) -> String {
     let lines = parse_lines(match_results);
+    // let teams: Vec<TallyStats> = Vec::new();
 
     for line in lines.iter() {
-        println!("{}\n", &line);
         let vals = parse_semi_color_separated_values(line.clone());
-        for val in vals.iter() {
-            print!("{}\n", &val);
-        }
+        println!("Parsed: {} {} {}\n", &vals[0], &vals[1], &vals[2]);
     }
-
 
     unimplemented!(
         "Given the result of the played matches '{}' return a properly formatted tally table string.",
