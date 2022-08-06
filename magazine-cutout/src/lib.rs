43b03a36f94244e50aeacaab9b7e23e7087996e1
Saad Ahmed <saadahm.siddiqui@gmail.com>
2022-08-06 23:36:42 +0500

    update u8

--- magazine-cutout/src/lib.rs
+++ magazine-cutout/src/lib.rs
@@ -5,10 +5,16 @@
 use std::collections::HashMap;
 
 pub fn can_construct_note(magazine: &Vec<&str>, note: &Vec<&str>) -> bool {
-    let megazine_map: HashMap<String, u8> = HashMap::new();
+    let mut megazine_map: HashMap<String, u8> = HashMap::new();
 
     for word in magazine {
+        let w = String::from(*word);
 
+        if (megazine_map.contains_key(&w)) {
+            megazine_map.entry(w).and_modify(|e| *e += 1);
+        } else {
+            megazine_map.insert(w, 1);
+        }
     }
     unimplemented!()
 }
