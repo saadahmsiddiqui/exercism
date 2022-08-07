455f564d9a9a80f592e3d535b0f42985fcdb954a
Saad Ahmed <saadahm.siddiqui@gmail.com>
2022-08-07 16:26:59 +0500

    cargo tests passed

--- magazine-cutout/src/lib.rs
+++ magazine-cutout/src/lib.rs
@@ -1,7 +1,3 @@
-// This stub file contains items that aren't used yet; feel free to remove this module attribute
-// to enable stricter warnings.
-#![allow(unused)]
-
 use std::collections::HashMap;
 
 pub fn can_construct_note(magazine: &Vec<&str>, note: &Vec<&str>) -> bool {
@@ -10,16 +6,47 @@ pub fn can_construct_note(magazine: &Vec<&str>, note: &Vec<&str>) -> bool {
     for word in magazine {
         let w = String::from(*word);
 
-        if (megazine_map.contains_key(&w)) {
+        if megazine_map.contains_key(&w) {
             megazine_map.entry(w).and_modify(|e| *e += 1);
         } else {
             megazine_map.insert(w, 1);
         }
     }
 
-    for (key, val) in megazine_map.iter() {
-        println!("{} {}", key, *val);
+    let mut note_words = note.len();
+
+    for word in note.iter() {
+        let note_w = String::from(*word);
+
+        let mut remove_key = false;
+        let mut decrease_count = false;
+        match megazine_map.get_key_value(&note_w) {
+            Some(key_match) => {
+                let (_word, count) = key_match;
+
+                if *count == 0 {
+                    remove_key = true;
+                } else {
+                    note_words -= 1;
+                    decrease_count = true;
+                }
+            },
+            None => return false
+        }
+        
+        if remove_key {
+            megazine_map.remove_entry(&note_w);
+        }
+
+        if decrease_count {
+            megazine_map.entry(note_w).and_modify(|e| *e -= 1);
+        }
     }
-    
-    unimplemented!()
+
+    if note_words == 0 {
+        return true;
+    }
+
+    return false;
+
 }
