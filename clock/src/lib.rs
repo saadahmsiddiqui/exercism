fd5e0bf39fcb150c373048c658150ca1701bb9d2
Saad Ahmed <saadahm.siddiqui@gmail.com>
2022-08-13 17:23:57 +0500

    up

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,12 +1,29 @@
-use std::{fmt};
+use std::{fmt, cmp};
 
 pub struct Clock {
     hours: i32,
     minutes: i32
 }
 
+impl cmp::PartialEq for Clock {
+    fn eq(&self, other: &Self) -> bool {
+        self.hours == other.hours && self.minutes == other.minutes
+    }
+}
+
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{:0>2}:{:0>2}",
+            self.hours,
+            self.minutes
+        )
+    }
+}
+
+impl fmt::Debug for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(
             f,
             "{:0>2}:{:0>2}",
