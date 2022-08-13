345a4a6262b580d14d7420d17a70ef20d4bd1c5a
Saad Ahmed <saadahm.siddiqui@gmail.com>
2022-08-13 15:32:57 +0500

    update lib

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,15 +1,44 @@
 pub struct Clock {
-    hours: u32,
-    minutes: u32
+    hours: i32,
+    minutes: i32
 }
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        unimplemented!(
-            "Construct a new Clock from {} hours and {} minutes",
-            hours,
-            minutes
-        );
+        let mut start_hours = match hours > 0 {
+            true => {
+                let mut start = 0;
+                let mut counter = hours;
+                while (counter > 0) {
+                    start = start + 1 % 23;
+                    counter = counter - 1;
+                }
+
+                start
+            },
+            false => {
+                let mut start = 0;
+                let mut counter = hours;
+                while (counter > 0) {
+                    start = start + 1 % 23;
+                    counter = counter - 1;
+                }
+
+                start
+            }
+        };
+
+        let start_minutes = match minutes > 0 {
+            true => {
+                0
+            },
+            false => {
+                0
+            }
+        };
+
+
+        Clock { hours: start_hours, minutes: start_minutes }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
