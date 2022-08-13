6948a807e4bf7246e39c8a80295cd1a29c4223ee
Saad Ahmed <saadahm.siddiqui@gmail.com>
2022-08-14 02:44:03 +0500

    refactor

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,8 +1,8 @@
-use std::{fmt, cmp};
+use std::{cmp, fmt};
 
 pub struct Clock {
     hours: i32,
-    minutes: i32
+    minutes: i32,
 }
 
 impl cmp::PartialEq for Clock {
@@ -13,23 +13,13 @@ impl cmp::PartialEq for Clock {
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "{:0>2}:{:0>2}",
-            self.hours,
-            self.minutes
-        )
+        write!(f, "{:0>2}:{:0>2}", self.hours, self.minutes)
     }
 }
 
 impl fmt::Debug for Clock {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "{:0>2}:{:0>2}",
-            self.hours,
-            self.minutes
-        )
+        write!(f, "{:0>2}:{:0>2}", self.hours, self.minutes)
     }
 }
 
@@ -40,63 +30,65 @@ fn add_hour_safe(current_hours: i32, hours: i32) -> i32 {
             let carried = hours.abs() % 24;
             (current_hours - carried + 24) % 24
         }
-    }    
+    }
 }
 
+fn calculate_minute_udpate(clock: &Clock, minutes: i32) -> (i32, i32) {
+    let mut current_clock_hours = clock.hours;
+    let mut current_clock_minutes = clock.minutes;
+
+    let remaining_minutes = match minutes > 0 {
+        true => minutes % 60,
+        false => minutes.abs() % 60,
+    };
+
+    let hours_to_add = minutes / 60;
+    current_clock_hours = add_hour_safe(current_clock_hours, hours_to_add);
+
+    match minutes > 0 {
+        true => {
+            if current_clock_minutes + remaining_minutes > 59 {
+                current_clock_minutes = (current_clock_minutes + remaining_minutes) % 60;
+                current_clock_hours = add_hour_safe(current_clock_hours, 1);
+            } else {
+                current_clock_minutes = current_clock_minutes + remaining_minutes;
+            }
+        }
+        false => {
+            if current_clock_minutes - remaining_minutes < 0 {
+                current_clock_hours = add_hour_safe(current_clock_hours, -1);
+                let minute_difference = (current_clock_minutes - remaining_minutes).abs();
+                current_clock_minutes = 60 - minute_difference;
+            } else {
+                current_clock_minutes = current_clock_minutes - remaining_minutes;
+            }
+        }
+    }
+
+    (current_clock_hours, current_clock_minutes)
+}
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let mut clock = Clock { hours: 0, minutes: 0 };
+        let mut clock = Clock {
+            hours: 0,
+            minutes: 0,
+        };
         clock = clock.add_hours(hours);
         clock = clock.add_minutes(minutes);
         clock
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        let mut curr_hours = self.hours;
-        let mut cl_minutes = self.minutes;
-
-        if minutes > 0 {
-            let remaining_minutes;
-
-            if minutes > 59 {
-                curr_hours = add_hour_safe(curr_hours, minutes / 60);
-                remaining_minutes = minutes % 60;
-            } else {
-                remaining_minutes = minutes;
-            }
-
-            if cl_minutes + remaining_minutes > 59 {
-                cl_minutes = (cl_minutes + remaining_minutes) % 60;
-                curr_hours = add_hour_safe(curr_hours, 1);
-            } else {
-                cl_minutes = cl_minutes + remaining_minutes;
-            }
-        } else {
-            let minutes_to_rewind = minutes.abs();
-            let carry_over_minutes;
-            
-            if minutes_to_rewind > 59 {
-                curr_hours = add_hour_safe(curr_hours, - (minutes_to_rewind / 60));
-                carry_over_minutes = minutes_to_rewind % 60
-            } else {
-                carry_over_minutes = minutes_to_rewind;
-            }
-
-            if cl_minutes - carry_over_minutes < 0 {
-                curr_hours = add_hour_safe(curr_hours, - 1);
-                let diff = (cl_minutes - carry_over_minutes).abs();
-                cl_minutes = 60 - diff;
-            } else {
-                cl_minutes = cl_minutes - carry_over_minutes;
-            }
-        }
-
-        return Clock { hours: curr_hours, minutes: cl_minutes };
+        let (hours, minutes) = calculate_minute_udpate(&self, minutes);
+        return Clock { hours, minutes };
     }
 
     pub fn add_hours(&self, hours: i32) -> Self {
         let new_hours = add_hour_safe(self.hours, hours);
-        Clock { hours: new_hours, minutes: self.minutes }
+        Clock {
+            hours: new_hours,
+            minutes: self.minutes,
+        }
     }
 }
