939412d1d94873b3271007879482e87b75401382
Saad Ahmed <saadahm.siddiqui@gmail.com>
2022-08-15 19:31:39 +0500

    grapheme

--- reverse-string/src/lib.rs
+++ reverse-string/src/lib.rs
@@ -1,7 +1,7 @@
 use unicode_segmentation::UnicodeSegmentation;
 
 pub fn reverse(input: &str) -> String {
-    let g = input.graphemes(true).collect::<Vec<&str>>();
+    let g = UnicodeSegmentation::graphemes(input, true).collect::<Vec<&str>>();
 
     // String::from_utf8(bts_vec).unwrap()
     String::from("")
