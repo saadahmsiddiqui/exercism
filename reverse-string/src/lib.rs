ec407bf6265ab68d7a7d68b2fab3565651a86d22
Saad Ahmed <saadahm.siddiqui@gmail.com>
2022-08-20 22:54:50 +0500

    lol

--- reverse-string/src/lib.rs
+++ reverse-string/src/lib.rs
@@ -1,17 +1,5 @@
-use std::str::FromStr;
-
 use unicode_segmentation::UnicodeSegmentation;
 
 pub fn reverse(input: &str) -> String {
-    let mut g = UnicodeSegmentation::graphemes(input, true).collect::<Vec<&str>>();
-    g.reverse();
-
-    let string_vc = g.iter().map(|ch| String::from(*ch)).collect::<Vec<String>>();
-    let mut final_string = String::from("");
-
-    for ch in string_vc.iter() {
-        final_string = final_string + ch;
-    }
-
-    final_string
+    UnicodeSegmentation::graphemes(input, true).rev().collect::<String>()
 }
