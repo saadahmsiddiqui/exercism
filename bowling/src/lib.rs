fb5044c1b89b873e4b67bac9a0a1958e91514979
Saad Ahmed <saadahm.siddiqui@gmail.com>
2022-08-21 00:40:08 +0500

    bw

--- bowling/src/lib.rs
+++ bowling/src/lib.rs
@@ -38,26 +38,27 @@ impl BowlingGame {
             return Err(Error::GameComplete);
         }
 
-        if self.frame == 0 && self.frame_hitory.len() == 0 {
-            let is_strike = pins == 10;
-            let mut first_frame = new_empty_frame_history();
-            first_frame.is_strike = is_strike;
-            first_frame.first_turn_pins_knocked = pins as u8;
-            self.frame_hitory.push(first_frame);
-            if is_strike {
+        if self.frame == 0 {
+            if self.frame_hitory.len() == 0 {
+                let is_strike = pins == 10;
+                let mut first_frame = new_empty_frame_history();
+                first_frame.is_strike = is_strike;
+                first_frame.first_turn_pins_knocked = pins as u8;
+                self.frame_hitory.push(first_frame);
+                if is_strike {
+                    self.frame = self.frame + 1;
+                }
+            } else {
+                self.frame_hitory[self.frame as usize].is_spare = true;
+                if self.frame_hitory[self.frame as usize].first_turn_pins_knocked + pins as u8 == 10 {
+                    self.frame_hitory[self.frame as usize].is_spare = false;
+                }
+                self.frame_hitory[self.frame as usize].second_turn_pins_knocked = pins as u8;
                 self.frame = self.frame + 1;
             }
         }
 
-        if self.frame == 0 && self.frame_hitory.len() != 0 {
-            self.frame_hitory[self.frame as usize].is_spare = true;
-            if self.frame_hitory[self.frame as usize].first_turn_pins_knocked + pins as u8 == 10 {
-                self.frame_hitory[self.frame as usize].is_spare = false;
-            }
-            self.frame_hitory[self.frame as usize].second_turn_pins_knocked = pins as u8;
-            self.frame = self.frame + 1;
-        }
-        
+
         unimplemented!("Return the score if the game is complete, or None if not.");
     }
 
