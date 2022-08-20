67510c79d99c631e980aa507cf0f9a37f7c9dd81
Saad Ahmed <saadahm.siddiqui@gmail.com>
2022-08-20 22:52:18 +0500

    hope there was a better way to do this 🤮

--- reverse-string/src/lib.rs
+++ reverse-string/src/lib.rs
@@ -1,8 +1,17 @@
+use std::str::FromStr;
+
 use unicode_segmentation::UnicodeSegmentation;
 
 pub fn reverse(input: &str) -> String {
-    let g = UnicodeSegmentation::graphemes(input, true).collect::<Vec<&str>>();
+    let mut g = UnicodeSegmentation::graphemes(input, true).collect::<Vec<&str>>();
+    g.reverse();
 
-    // String::from_utf8(bts_vec).unwrap()
-    String::from("")
+    let string_vc = g.iter().map(|ch| String::from(*ch)).collect::<Vec<String>>();
+    let mut final_string = String::from("");
+
+    for ch in string_vc.iter() {
+        final_string = final_string + ch;
+    }
+
+    final_string
 }
