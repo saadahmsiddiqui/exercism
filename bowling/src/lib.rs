bbcd22e00fe5986c026ef153e6d87b987d28a236
Saad Ahmed <saadahm.siddiqui@gmail.com>
2022-08-22 20:05:16 +0500

    yuck

--- bowling/src/lib.rs
+++ bowling/src/lib.rs
@@ -21,12 +21,12 @@ fn new_empty_frame_history() -> FrameHistory {
 }
 pub struct BowlingGame {
     frame: u8,
-    frame_hitory: Vec<FrameHistory>,
+    frame_history: Vec<FrameHistory>,
 }
 
 impl BowlingGame {
     pub fn new() -> Self {
-        BowlingGame { frame: 0, frame_hitory: Vec::<FrameHistory>::with_capacity(10) }
+        BowlingGame { frame: 0, frame_history: Vec::<FrameHistory>::with_capacity(10) }
     }
 
     pub fn roll(&mut self, pins: u16) -> Result<(), Error> {
@@ -34,31 +34,46 @@ impl BowlingGame {
             return Err(Error::InvalidNumberOfPins);            
         }
 
-        if self.frame + 1 > 10 {
+        if self.frame + 1 >= 10 {
             return Err(Error::GameComplete);
         }
 
         if self.frame == 0 {
-            if self.frame_hitory.len() == 0 {
+            if self.frame_history.len() == 0 {
                 let is_strike = pins == 10;
                 let mut first_frame = new_empty_frame_history();
                 first_frame.is_strike = is_strike;
                 first_frame.first_turn_pins_knocked = pins as i8;
-                self.frame_hitory.push(first_frame);
+                self.frame_history.push(first_frame);
                 if is_strike {
                     self.frame = self.frame + 1;
                 }
             } else {
-                self.frame_hitory[self.frame as usize].is_spare = true;
-                if self.frame_hitory[self.frame as usize].first_turn_pins_knocked + pins as i8 == 10 {
-                    self.frame_hitory[self.frame as usize].is_spare = false;
+                self.frame_history[self.frame as usize].is_spare = false;
+                if self.frame_history[self.frame as usize].first_turn_pins_knocked + pins as i8 == 10 {
+                    self.frame_history[self.frame as usize].is_spare = true;
                 }
-                self.frame_hitory[self.frame as usize].second_turn_pins_knocked = pins as i8;
+                self.frame_history[self.frame as usize].second_turn_pins_knocked = pins as i8;
+                self.frame = self.frame + 1;
+            }
+        } else {
+            let last_frame = (self.frame - 1) as usize;
+            let is_complete_last_frame = self.frame_history[last_frame].second_turn_pins_knocked != -1;
+            if is_complete_last_frame {
+                let mut new_current_frame = new_empty_frame_history();
+                new_current_frame.is_strike = pins == 10;
+                new_current_frame.is_spare = false;
+                new_current_frame.first_turn_pins_knocked = pins as i8;
+                self.frame_history[self.frame as usize] = new_current_frame;
+            } else {
+                self.frame_history[self.frame as usize].is_spare = false;
+                if self.frame_history[self.frame as usize].first_turn_pins_knocked + pins as i8 == 10 {
+                    self.frame_history[self.frame as usize].is_spare = true;
+                }
+                self.frame_history[self.frame as usize].second_turn_pins_knocked = pins as i8;
                 self.frame = self.frame + 1;
             }
         }
-
-
         unimplemented!("Return the score if the game is complete, or None if not.");
     }
 
