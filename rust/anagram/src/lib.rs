dedaba8abcb03333ceaece9376f2c8f615fea703
Saad Ahmed <saadahm.siddiqui@gmail.com>
2022-11-20 06:02:20 +0500

    Anagram alog

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,9 +1,27 @@
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    unimplemented!(
-        "For the '{}' word find anagrams among the following words: {:?}",
-        word,
-        possible_anagrams
-    );
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    let mut anagrams = HashSet::new();
+
+    for anagram in possible_anagrams {
+        let word_chars: Vec<char> = word.chars().map(|x| x.to_ascii_lowercase()).collect();
+        let anagram_chars: Vec<char> = anagram.chars().map(|x| x.to_ascii_lowercase()).collect();
+
+        if word_chars.len() != anagram_chars.len() {
+            continue;
+        }
+
+        let mut all_match = true;
+        for ch in anagram_chars.iter() {
+            if !word_chars.contains(&ch) {
+                all_match = false;
+            }
+        }
+
+        if all_match {
+            anagrams.insert(anagram.clone());
+        }
+    }
+
+    return anagrams;
 }
