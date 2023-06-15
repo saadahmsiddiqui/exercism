85b77c905f6ea5f2019f5907e3f9572c83ebbccc
Saad Ahmed <saadahm.siddiqui@gmail.com>
2023-06-16 03:26:42 +0500

    Rust

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,41 +1,26 @@
 use std::collections::HashSet;
 
+use unicode_segmentation::UnicodeSegmentation;
+
+/// Convert a word into a sorted list of unicode graphemes
+
+fn convert_to_anagram(word: &str) -> Vec<String> {
+    let mut result: Vec<String> = word.graphemes(true).map(|c| c.to_uppercase()).collect();
+
+    result.sort();
+
+    result
+}
+
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
-    let mut anagrams = HashSet::new();
+    let word_anagram = convert_to_anagram(word);
 
-    for anagram in possible_anagrams {
-        let word_chars: Vec<char> = word.chars().map(|x| x.to_ascii_lowercase()).collect();
-        let anagram_chars: Vec<char> = anagram.chars().map(|x| x.to_ascii_lowercase()).collect();
-
-        if word_chars.len() != anagram_chars.len() {
-            continue;
-        }
-
-        if word_chars.len() == anagram_chars.len() {
-            let mut all_match = true;
-
-            for i in 0..word_chars.len() {
-                if word_chars.get(i) != anagram_chars.get(i) {
-                    all_match = false;
-                }
-            }
-
-            if all_match {
-                continue;
-            }
-        }
-
-        let mut all_exist = true;
-        for ch in anagram_chars.iter() {
-            if !word_chars.contains(&ch) {
-                all_exist = false;
-            }
-        }
-
-        if all_exist {
-            anagrams.insert(anagram.clone());
-        }
-    }
-
-    return anagrams;
+    possible_anagrams
+        .iter()
+        .cloned() // converts &a to a
+        .filter(|anagram| {
+            convert_to_anagram(anagram) == word_anagram
+                && word.to_uppercase() != anagram.to_uppercase()
+        })
+        .collect()
 }
